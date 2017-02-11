151d56cd51a7cbeb6a4b22cf3c3fa831c1758463
In-somnia <kuzkina.julia@gmail.com>
2017-02-11 15:02:59 +0300

    io task3 + renewed settings

--- io/src/main/java/task3/EncodingConversion.java
+++ io/src/main/java/task3/EncodingConversion.java
@@ -2,6 +2,8 @@ package task3;
 
 
 import lombok.SneakyThrows;
+
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,8 +14,8 @@ public class EncodingConversion {
     @SneakyThrows
     public static void main(String[] args) {
 
-        List<String> lines = Files.readAllLines(Paths.get("D:\\test6"), StandardCharsets.UTF_8);
-        Files.write(Paths.get("D:\\test7.txt"), lines, StandardCharsets.UTF_16);
+            List<String> lines = Files.readAllLines(Paths.get("D:\\test6"), StandardCharsets.UTF_8);
+            Files.write(Paths.get("D:\\test7.txt"), lines, StandardCharsets.UTF_16);
 
     }
 }
